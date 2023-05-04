09ae788cc8c7218b8db1cbf72ab8bdce4ebdfa8f
Julian Betts <wjulianbetts@gmail.com>
2023-05-04 11:45:45 -0700

    time-block feature

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const currentEl = document.querySelector("#date")
 
 currentEl.textContent = moment().format('LL')
 
+
+
+
+var currentHour = moment().hours()
+var timeblock = document.querySelectorAll('.time-block')
+
+timeblock.forEach(element => {
+    var id = Number(element.getAttribute('id').split('-')[1])
+    if (id < currentHour) {
+        element.classList.add('past')
+    }
+    else if (id == currentHour) {
+        element.classList.add('present')
+    }
+    else {
+        element.classList.add('future')
+    }
+});
+
+//save button, auto save on browser
